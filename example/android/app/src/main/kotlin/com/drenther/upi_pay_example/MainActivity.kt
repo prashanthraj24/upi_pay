fa655f1e4277a7a36140d00d6450034c58c8237a
Hemant Patel <hmtp140789@gmail.com>
2022-04-05 15:23:05 +0530

    Build error fix

--- example/android/app/src/main/kotlin/com/drenther/upi_pay_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/drenther/upi_pay_example/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.drenther.upi_pay_example
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(flutterEngine!!)
+    }
 }
